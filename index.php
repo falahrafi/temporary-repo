5d60ef4a52e8ad690a00f430b225daa7e0193b32
Falah Rafi <45087250+falahrafi@users.noreply.github.com>
2023-04-11 12:08:33 +0700

    update 1

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
-<h1>Hello World!</h1>
-<hr>
-Nama: Muhammad Falah Abdurrafi
-NIM: 20.01.53.0033
+<?php
+echo "<h1>Hello World!</h1>";
+echo "<hr>";
+echo "Nama: Muhammad Falah Abdurrafi<br>";
+echo "NIM: 20.01.53.0033";
+
